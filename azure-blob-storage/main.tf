214ec372c8cdda8503920b8f99893964453714cd
klaudiastarczew <114864973+klaudiastarczew@users.noreply.github.com>
2023-06-04 21:04:34 +0200

    Update main.tf

--- azure-blob-storage/main.tf
+++ azure-blob-storage/main.tf
@@ -1,26 +1,26 @@
-resource "azurerm_resource_group" "klaudias" {
-  name     = var.name_klaudias_resources
+resource "azurerm_resource_group" "app" {
+  name     = var.name_app_resources
   location = var.location
 }
 
-resource "azurerm_storage_account" "klaudias" {
+resource "azurerm_storage_account" "app" {
   name                     = var.name_storage_account
-  resource_group_name      = azurerm_resource_group.klaudias.name
-  location                 = azurerm_resource_group.klaudias.location
+  resource_group_name      = azurerm_resource_group.app.name
+  location                 = azurerm_resource_group.app.location
   account_tier             = var.account_tier
   account_replication_type = var.account_replication_type
 }
 
-resource "azurerm_storage_container" "klaudias" {
+resource "azurerm_storage_container" "app" {
   name                  = var.name_storage_container
-  storage_account_name  = azurerm_storage_account.klaudias.name
+  storage_account_name  = azurerm_storage_account.app.name
   container_access_type = var.access_container_type
 }
 
-resource "azurerm_storage_blob" "klaudias" {
+resource "azurerm_storage_blob" "app" {
   name                   = var.name_storage_blob
-  storage_account_name   = azurerm_storage_account.klaudias.name
-  storage_container_name = azurerm_storage_container.klaudias.name
+  storage_account_name   = azurerm_storage_account.app.name
+  storage_container_name = azurerm_storage_container.app.name
   type                   = var.type_storage_blob
   source                 = var.source_storage_blob
 }
