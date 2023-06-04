66153e84a157adf8e2fed0a0e3fecd82affd647e
klaudiastarczew <114864973+klaudiastarczew@users.noreply.github.com>
2023-06-04 21:12:12 +0200

    Update main.tf

--- azure-functions/main.tf
+++ azure-functions/main.tf
@@ -1,20 +1,20 @@
-resource "azurerm_resource_group" "klaudias" {
+resource "azurerm_resource_group" "app" {
   name     = var.name_resource_group
   location = var.location
 }
 
-resource "azurerm_storage_account" "klaudias" {
+resource "azurerm_storage_account" "app" {
   name                     = var.name_storage_account
-  resource_group_name      = azurerm_resource_group.klaudias.name
-  location                 = azurerm_resource_group.klaudias.location
+  resource_group_name      = azurerm_resource_group.app.name
+  location                 = azurerm_resource_group.app.location
   account_tier             = var.storage_account_tier
   account_replication_type = var.storage_account_replication_type
 }
 
-resource "azurerm_app_service_plan" "klaudias" {
+resource "azurerm_app_service_plan" "app" {
   name                = var.name_app_service_plan
-  location            = azurerm_resource_group.klaudias.location
-  resource_group_name = azurerm_resource_group.klaudias.name
+  location            = azurerm_resource_group.app.location
+  resource_group_name = azurerm_resource_group.app.name
 
   sku {
     tier = var.tier_sku
@@ -22,25 +22,25 @@ resource "azurerm_app_service_plan" "klaudias" {
   }
 }
 
-resource "azurerm_linux_function_app" "klaudias" {
+resource "azurerm_linux_function_app" "app" {
   name                = var.name_linux_function_app
-  resource_group_name = azurerm_resource_group.klaudias.name
-  location            = azurerm_resource_group.klaudias.location
+  resource_group_name = azurerm_resource_group.app.name
+  location            = azurerm_resource_group.app.location
 
-  storage_account_name = azurerm_storage_account.klaudias.name
-  service_plan_id      = azurerm_service_plan.klaudias.id
+  storage_account_name = azurerm_storage_account.app.name
+  service_plan_id      = azurerm_service_plan.app.id
 
   site_config {}
 }
 
-resource "azurerm_function_app_function" "klaudias" {
+resource "azurerm_function_app_function" "app" {
   name                       = var.name_test_azure_functions
   id                         = var.id_function_app_function
-  location                   = azurerm_resource_group.klaudias.location
-  resource_group_name        = azurerm_resource_group.klaudias.name
-  app_service_plan_id        = azurerm_app_service_plan.klaudias.id
-  storage_account_name       = azurerm_storage_account.klaudias.name
-  storage_account_access_key = azurerm_storage_account.klaudias.primary_access_key
+  location                   = azurerm_resource_group.app.location
+  resource_group_name        = azurerm_resource_group.app.name
+  app_service_plan_id        = azurerm_app_service_plan.app.id
+  storage_account_name       = azurerm_storage_account.app.name
+  storage_account_access_key = azurerm_storage_account.app.primary_access_key
   config_json                = ""
   function_app_id            = ""
 }
